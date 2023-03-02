430511896349a6b553f9286e39649fadfb6d1d88
Smart Irriagtion <115499393+digiworld01@users.noreply.github.com>
2023-03-02 19:35:03 +0500

    Add files via upload

---
+++ CODE.ino
@@ -0,0 +1,187 @@
+
+//   ********CODE BY KHURAFATI BABA (SARTHAK MISHRA)**********
+//   ********CODE BY KHURAFATI BABA (SARTHAK MISHRA)**********
+
+
+
+#include <AFMotor.h>                          //For adding the library of shield
+#include<Servo.h>                             //For adding the library of servo motor
+Servo m1;                                     //Variable of servo motor 1
+Servo m2;                                     //variable of servo motor 2
+int pos;                                      //Variable that will define the postion of servos
+const int trigPin = A0;                       //Defines the trig pin of ultrasonic sensor 1st 
+const int echoPin = A1;                       //Defines the echo pin of ultrasonic sensor 1st
+const int trigPin1=A2;                        //Defines the trig pin of ultrasonic sensor 2nd 
+const int echoPin1=A3;                        //Defines the echo pin of ultrasonic sensor 2nd
+int mpin = A4;                                //Defines the pin of moisture sensor pin
+int mout;                                     //Variable to store the value given by moisture sensor
+long duration, duration1;                     //Variable that stores the duration value given by ultrasonic sensor
+int distance, distance1;                      //Variable that stores the distance value calculated by the formaula
+
+
+AF_DCMotor motor1(1, MOTOR12_1KHZ);           //Defines the frequency which will be given to motor 1
+AF_DCMotor motor2(2, MOTOR12_1KHZ);           //Defines the frequency which will be given to motor 2
+AF_DCMotor motor4(4, MOTOR12_1KHZ);
+
+void setup() {
+  Serial.begin(9600);                         //starts serial communication with the arduino and PC
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin1, OUTPUT); 
+  pinMode(echoPin1, INPUT);
+  m1.attach(10);                             //Define the attached pin for servo motor 1
+  m2.attach(9);                              //Define the attached pin for servo motor 1
+
+  m1.write(0);
+  m2.write(120);
+  
+  motor1.setSpeed(255);                      //To set the particular speed of motor 1
+  motor2.setSpeed(255);                      //To set the particular speed of motor 2
+}
+
+void loop() 
+{
+ 
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance= duration*0.034/2;
+digitalWrite(trigPin1, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin1, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin1, LOW);
+duration1 = pulseIn(echoPin1, HIGH);
+distance1= duration1*0.034/2;
+
+
+  if (distance>=15)
+ {
+ 
+   Forward();
+   delay(100);
+ }
+
+
+ else if(distance<15)
+
+{
+  Stop();
+  delay(70);
+  
+  digitalWrite(trigPin1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin1, LOW);
+  duration1 = pulseIn(echoPin1, HIGH);
+  distance1= duration1*0.034/2;
+
+   if(distance1>=6)
+  {
+   servoF();
+   delay(100);
+   }
+}
+   
+   mout=analogRead(mpin);
+  
+ 
+  if(distance1<6 && mout<=500)
+  
+{
+  
+  servoB();
+  delay(100);
+
+  Forward();
+  delay(1000);
+  
+}
+
+  if(distance1<6 && mout>500)
+ 
+ {
+   
+ while(mout>500)
+ {
+  
+  motor4.run(FORWARD);
+
+  mout=analogRead(mpin);
+
+  if(mout<=500)
+  {
+  motor4.run(RELEASE);
+  delay(100);
+  break;
+  }
+  
+ }
+ }
+ }
+void Stop()
+
+{
+  
+
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  
+}
+
+void Forward()
+
+{
+
+  
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  
+}
+void servoF()
+{
+  
+  Serial.println("servo forward is fine");
+  delay(100);
+  
+for (pos=0;pos<=120;pos+=1)
+
+{
+ 
+  digitalWrite(trigPin1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin1, LOW);
+  duration1 = pulseIn(echoPin1, HIGH);
+  distance1= duration1*0.034/2;
+
+ 
+ if(distance1>6)
+  {
+    m1.write(pos);
+    m2.write(120-pos);
+  }
+
+}
+
+}
+
+void servoB()
+{
+  Serial.println("Servo back is fine");
+  delay(100);
+  
+  for(pos=120;pos>=0;pos-=1)
+  {
+    {
+    m1.write(pos);
+    m2.write(120-pos);
+    }
+    delay(30);
+  }
+}                              
